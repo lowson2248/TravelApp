1d471d74eb26cdccbf835220dd991dfee624c5aa
toshikihayashi <toshikihayashi4004@gmail.com>
2019-07-04 11:48:57 +0900

    権限検索追加(アンケート一覧画面)

--- travel/src/main/java/com/travel/service/MemberServiceImpl.java
+++ travel/src/main/java/com/travel/service/MemberServiceImpl.java
@@ -1,5 +1,31 @@
 package com.travel.service;
 
-public class MemberServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
+import com.travel.model.Member;
+import com.travel.repository.MemberRepository;
+import com.travel.repository.ProjectRepository;
+import com.travel.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+	
+	@Autowired
+	MemberRepository memberRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	ProjectRepository projectRepository;
+	
+	public Member findByUserDetailsAndProjectId(UserDetails userDetails,int projectId ) {
+		return memberRepository.findByUserAndProject(userRepository.findByMailAddress(userDetails.getUsername()),projectRepository.findByProjectId(projectId));
+	}
 }
